f51dd7415565ddd7a3435561b6d81be7848acfd3
James Golick <jamesgolick@gmail.com>
2011-04-21 12:31:13 -0700

    depend on jerkson

--- project/build/StatsdProject.scala
+++ project/build/StatsdProject.scala
@@ -9,9 +9,14 @@ class StatsdProject(info: ProjectInfo) extends DefaultProject(info) {
 
   val jbossRepo = "JBoss Repo" at
                    "https://repository.jboss.org/nexus/content/repositories/releases"
+  val fetlifeRepo = "FetLife Repo" at
+                     "http://mvn.dal.fetlife"
 
   val metrics  = "com.yammer" %% "metrics" % "1.0.7" withSources()
   val specs    = "org.scala-tools.testing" %% "specs" % "1.6.5"
   val mockito  = "org.mockito" % "mockito-all" % "1.8.5"
   val netty    = "org.jboss.netty" % "netty" % "3.2.4.Final" withSources()
+  val jerkson  = "com.codahale" %% "jerkson" % "0.1.5-SNAPSHOT"
+
+  override def mainClass = Some("bitlove.statsd.server.StatsdServer")   
 }
