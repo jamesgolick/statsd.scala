597704971918c6f476ae092222c0a562f84e5078
James Golick <jamesgolick@gmail.com>
2011-04-21 14:05:09 -0700

    add a bunch of logging

--- src/main/scala/server/StatsdServerHandler.scala
+++ src/main/scala/server/StatsdServerHandler.scala
@@ -4,26 +4,41 @@ import bitlove.statsd.Statsd
 
 import com.codahale.jerkson.Json._
 
+import com.codahale.logula.Logging
+
 import org.jboss.netty.channel.ChannelHandlerContext
 import org.jboss.netty.channel.ExceptionEvent
 import org.jboss.netty.channel.MessageEvent
 import org.jboss.netty.channel.SimpleChannelUpstreamHandler
 
 class StatsdServerHandler(stats: Statsd)
-  extends SimpleChannelUpstreamHandler {
+  extends SimpleChannelUpstreamHandler with Logging {
   override def messageReceived(ctx: ChannelHandlerContext, e: MessageEvent) = {
     val msg    = e.getMessage.asInstanceOf[String]
     val metric = parse[Map[String, String]](msg)
+    val name   = metric("name")
+
+    log.fine(msg)
 
     metric("action") match {
       case "inc" => 
-        stats.incrementCounter(metric("name"), metric("delta").toLong)
+        val delta = metric("delta").toLong
+        log.fine("Increment counter %s with %d.", name, delta)
+        stats.incrementCounter(name, delta)
       case "dec" =>
-        stats.decrementCounter(metric("name"), metric("delta").toLong)
+        val delta = metric("delta").toLong
+        log.fine("Decrement counter %s with %d.", name, delta)
+        stats.decrementCounter(name, delta)
       case "timing" =>
-        stats.addTiming(metric("name"), metric("duration").toInt)
+        val duration = metric("duration").toInt
+        log.fine("Add timing for %s with %d.", name, duration)
+        stats.addTiming(name, duration)
       case "mark" =>
-        stats.markLoadMeter(metric("name"), metric("count").toInt)
+        val count = metric("count").toInt
+        log.fine("Mark load meter %s with %d", name, count)
+        stats.markLoadMeter(name, count)
+      case x: String => 
+        log.severe("Unknown action: %s", x)
     }
   }
 
